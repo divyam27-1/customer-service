03c689f830e7716e792e72631dddb673188ccb47
Divyam Tiwari <divyam2701@gmail.com>
2026-06-04 15:32:17 +0530

    Initial Commit

---
+++ CustomerServicePortal/src/test/java/com/mphasis/csp/CustomerServicePortal/CustomerServicePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mphasis.csp.CustomerServicePortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServicePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
