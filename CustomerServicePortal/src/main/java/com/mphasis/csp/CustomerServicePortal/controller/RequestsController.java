4264a4c8a0e4558861c36c0ef560fe2caf534630
Divyam Tiwari <divyam2701@gmail.com>
2026-06-08 11:06:46 +0530

    Impl of requets ticket controller

--- CustomerServicePortal/src/main/java/com/mphasis/csp/CustomerServicePortal/controller/RequestsController.java
+++ CustomerServicePortal/src/main/java/com/mphasis/csp/CustomerServicePortal/controller/RequestsController.java
@@ -1,4 +1,21 @@
 package com.mphasis.csp.CustomerServicePortal.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class RequestsController {
+
+    @PostMapping("/raiseTicket")
+    public ResponseEntity<String> raiseTicket(@RequestBody String request) {
+        // Service Layer
+        return ResponseEntity.ok("Ticket Raised Successfully");
+    }
+
+    @PutMapping("/editTicket")
+    public ResponseEntity<String> editTicket(@RequestBody String request) {
+        // Service Layer
+        return ResponseEntity.ok("Ticket Edited Successfully");
+    }
 }
--- CustomerServicePortal/src/main/java/com/mphasis/csp/CustomerServicePortal/dto/RaiseTicketDTO.java
+++ CustomerServicePortal/src/main/java/com/mphasis/csp/CustomerServicePortal/dto/RaiseTicketDTO.java
@@ -1,4 +1,14 @@
 package com.mphasis.csp.CustomerServicePortal.dto;
 
+import com.mphasis.csp.CustomerServicePortal.enums.TicketCategory;
+import com.mphasis.csp.CustomerServicePortal.enums.TicketSubcategory;
+import lombok.Data;
+
+@Data
 public class RaiseTicketDTO {
+
+    private Long userId;
+    private TicketCategory category;
+    private TicketSubcategory subcategory;
+    private String description;
 }
